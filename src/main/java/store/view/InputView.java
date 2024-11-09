23e9029f7083db83a7b9276493229df0d9767221
이준범 <jach1206@naver.com>
2024-11-09 21:04:55 +0900

    feat(view): 사용자에게 입력을 받는 메서드 구현
    - inputGetAdditionalProduct 메서드: 추가로 물품을 더 받을지 말지를 입력받는 메서드
    
    - inputNonPromotion 메서드: 할인이 적용되지 않아도 구매할 것인지 물어보는 메서드

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -20,6 +20,20 @@ public class InputView {
         return validateFormat(Console.readLine());
     }
 
+    public String inputGetAdditionalProduct(String name, int count) {
+        System.out.println(NEW_LINE + "현재 " + name + "은(는) " + count + "개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
+        String answer = Console.readLine();
+        validateAnswer(answer);
+        return answer;
+    }
+
+    public String inputNonPromotion(String name, int count) {
+        System.out.println(NEW_LINE + "현재 " + name + " " + count + "개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)");
+        String answer = Console.readLine();
+        validateAnswer(answer);
+        return answer;
+    }
+
     private List<BuyingInformation> validateFormat(String input) {
         String[] allInformation = input.split(SEPARATOR);
         List<BuyingInformation> buyingInformation = new ArrayList<>();
@@ -38,5 +52,11 @@ public class InputView {
         }
 
         return new BuyingInformation(matcher.group(1), Integer.parseInt(matcher.group(2)));
+    }
+
+    private void validateAnswer(String answer) {
+        if (!(answer.equals("Y") || answer.equals("N"))) {
+            throw new IllegalArgumentException(InputFormatErrorMessage.IS_NOT_CORRECT_ANSWER.getMessage());
+        }
     }
 }
--- src/main/java/store/view/exception/InputFormatErrorMessage.java
+++ src/main/java/store/view/exception/InputFormatErrorMessage.java
@@ -2,7 +2,8 @@ package store.view.exception;
 
 public enum InputFormatErrorMessage {
 
-    IS_NOT_CORRECT_FORMAT("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+    IS_NOT_CORRECT_FORMAT("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."),
+    IS_NOT_CORRECT_ANSWER("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
 
     private final String message;
 
