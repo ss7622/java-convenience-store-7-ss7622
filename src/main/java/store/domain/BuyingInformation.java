7988d67a55e21228d8d2df587436fd9eca7b6f74
이준범 <jach1206@naver.com>
2024-11-09 21:08:50 +0900

    feat(domain): 필드 변경 및 각종 메서드 추가
    - 일반 상품 개수와 프로모션 상품 개수 필드를 추가했다.
    
    - 구매한 상품의 개수를 조정하는 메서드를 추가했다.
    
    - get 관련 메서드를 추가했다.

--- src/main/java/store/domain/BuyingInformation.java
+++ src/main/java/store/domain/BuyingInformation.java
@@ -5,19 +5,41 @@ import store.domain.exception.BuyingErrorMessage;
 public class BuyingInformation {
 
     private final String name;
-    private final int count;
+    private int normalCount = 0;
+    private int promotionCount = 0;
 
-    public BuyingInformation(String name, int count) {
-        checkInventory(name, count);
+    public BuyingInformation(String name, int normalCount) {
+        checkInventory(name, normalCount);
         this.name = name;
-        this.count = count;
+        this.normalCount = normalCount;
+    }
+
+    public void adjustCount(int normalCount, int promotionCount) {
+        this.normalCount = normalCount;
+        this.promotionCount = promotionCount;
+    }
+
+    public void addPromotionCount(int count) {
+        this.promotionCount += count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNormalCount() {
+        return normalCount;
+    }
+
+    public int getPromotionCount() {
+        return promotionCount;
     }
 
     private void checkInventory(String name, int count) {
         if (!Products.isExist(name)) {
             throw new IllegalArgumentException(BuyingErrorMessage.IS_NOT_EXIST.getMessage());
         }
-        int quantity = Products.getQuantity(name);
+        int quantity = Products.getAllQuantity(name);
         if (quantity < count) {
             throw new IllegalArgumentException(BuyingErrorMessage.OVER_COUNT.getMessage());
         }
