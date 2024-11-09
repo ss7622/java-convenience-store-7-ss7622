33bd996ae2fe6bcdc700bc7580e697c3ec7072c2
이준범 <jach1206@naver.com>
2024-11-09 21:21:29 +0900

    feat(view): 멤버십 할인을 입력받는 메서드 구현

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -11,6 +11,7 @@ import store.view.exception.InputFormatErrorMessage;
 public class InputView {
 
     private static final String BUYING_GUIDE_MESSAGE = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
+    private static final String MEMBERSHIP_GUIDE_MESSAGE = "멤버십 할인을 받으시겠습니까? (Y/N)";
     private static final String SEPARATOR = ",";
     private static final String NEW_LINE = "\n";
     private static final String BUYING_INFORMATION_PATTERN = "\\[([가-힣]+)-([0-9]+)\\]";
@@ -22,16 +23,17 @@ public class InputView {
 
     public String inputGetAdditionalProduct(String name, int count) {
         System.out.println(NEW_LINE + "현재 " + name + "은(는) " + count + "개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
-        String answer = Console.readLine();
-        validateAnswer(answer);
-        return answer;
+        return validateAnswer(Console.readLine());
     }
 
     public String inputNonPromotion(String name, int count) {
         System.out.println(NEW_LINE + "현재 " + name + " " + count + "개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)");
-        String answer = Console.readLine();
-        validateAnswer(answer);
-        return answer;
+        return validateAnswer(Console.readLine());
+    }
+
+    public String inputMembership() {
+        System.out.println(MEMBERSHIP_GUIDE_MESSAGE);
+        return validateAnswer(Console.readLine());
     }
 
     private List<BuyingInformation> validateFormat(String input) {
@@ -54,9 +56,10 @@ public class InputView {
         return new BuyingInformation(matcher.group(1), Integer.parseInt(matcher.group(2)));
     }
 
-    private void validateAnswer(String answer) {
+    private String validateAnswer(String answer) {
         if (!(answer.equals("Y") || answer.equals("N"))) {
             throw new IllegalArgumentException(InputFormatErrorMessage.IS_NOT_CORRECT_ANSWER.getMessage());
         }
+        return answer;
     }
 }
