ce7fe8cfb0e54b3134727385a549b0a806d0bc3f
이준범 <jach1206@naver.com>
2024-11-09 21:12:59 +0900

    feat(service): 프로모션과 관련된 각종 작업을 하는 클래스 생성
    - cantApplyPromotionCount: 프로모션 적용이 안 되는 상품의 개수를 반환하는 메서드이다.
    
    - divideNormalAndPromotionProduct: 구매한 상품이 프로모션 기간 중에 있다면,
    프로모션 재고와 일반 재고로 개수를 나누어 저장하는 메서드이다.
    
    - canGetFreeCount: 프로모션 기간 중에 있는 상품을 구매했을 시,
    무료로 더 받을 수 있는 상품의 개수를 반환하는 메서드이다.

---
+++ src/main/java/store/service/PromotionManager.java
@@ -0,0 +1,58 @@
+package store.service;
+
+import store.domain.BuyingInformation;
+import store.domain.Products;
+import store.domain.Promotion;
+
+public class PromotionManager {
+    // 프로모션 기간 중인지 확인 후, 프로모션 적용이 안 되는 상품의 개수를 반환
+    public int cantApplyPromotionCount(BuyingInformation buyingInformation) {
+        Promotion promotion = checkPromotion(buyingInformation);
+        int promotionQuantity = Products.getPromotionQuantity(buyingInformation.getName());
+        if (promotion == null ||
+                promotionQuantity > buyingInformation.getPromotionCount()) {
+            return 0;
+        }
+        return promotionQuantity % (promotion.getBuy() + promotion.getGet());
+    }
+
+    // 구매한 상품이 프로모션 기간 중에 있다면, 프로모션 재고와 일반 재고로 갯수를 나누는 작업
+    public void divideNormalAndPromotionProduct(BuyingInformation buyingInformation) {
+        if (checkPromotion(buyingInformation) == null) {
+            return;
+        }
+        int totalCount = buyingInformation.getNormalCount();
+        int promotionQuantity = Products.getPromotionQuantity(buyingInformation.getName());
+        if (totalCount < promotionQuantity) {
+            buyingInformation.adjustCount(0, totalCount);
+            return;
+        }
+        buyingInformation.adjustCount(totalCount - promotionQuantity, promotionQuantity);
+    }
+
+    // 무료로 더 받을 수 있는 프로모션 상품의 개수를 체크하는 메서드
+    public int canGetFreeCount(BuyingInformation buyingInformation) {
+        Promotion promotion = checkPromotion(buyingInformation);
+        if (promotion == null) {
+            return 0;
+        }
+        if (checkCanGive(buyingInformation, promotion)) {
+            return promotion.getGet();
+        }
+        return 0;
+    }
+
+    private Promotion checkPromotion(BuyingInformation buyingInformation) {
+        String promotionName = Products.getPromotion(buyingInformation.getName());
+        return Promotion.checkPromotion(promotionName);
+    }
+
+    // 상품을 추가로 더 줄 수 있는지 재고를 확인하는 메서드
+    private boolean checkCanGive(BuyingInformation buyingInformation, Promotion promotion) {
+        int remain = buyingInformation.getPromotionCount() % (promotion.getGet() + promotion.getBuy());
+        int totalGiveCount = buyingInformation.getPromotionCount() + promotion.getGet();
+
+        return remain == promotion.getBuy() &&
+                totalGiveCount <= Products.getPromotionQuantity(buyingInformation.getName());
+    }
+}
