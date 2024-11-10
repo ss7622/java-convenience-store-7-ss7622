fe8b36eb61bcbc37ce71a10db4c8e947dbd16933
이준범 <jach1206@naver.com>
2024-11-10 21:26:18 +0900

    style(view): 출력 형식에 맞춰 개선

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -24,22 +24,46 @@ public class InputView {
 
     public String inputGetAdditionalProduct(String name, int count) {
         System.out.println(NEW_LINE + "현재 " + name + "은(는) " + count + "개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
-        return validateAnswer(Console.readLine());
+        while (true) {
+            try {
+                return validateAnswer(Console.readLine());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public String inputNonPromotion(String name, int count) {
         System.out.println(NEW_LINE + "현재 " + name + " " + count + "개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)");
-        return validateAnswer(Console.readLine());
+        while (true) {
+            try {
+                return validateAnswer(Console.readLine());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public String inputMembership() {
-        System.out.println(MEMBERSHIP_GUIDE_MESSAGE);
-        return validateAnswer(Console.readLine());
+        System.out.println(NEW_LINE + MEMBERSHIP_GUIDE_MESSAGE);
+        while (true) {
+            try {
+                return validateAnswer(Console.readLine());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public String inputRetry() {
-        System.out.println(RETRY_GUIDE_MESSAGE);
-        return validateAnswer(Console.readLine());
+        System.out.println(NEW_LINE + RETRY_GUIDE_MESSAGE + NEW_LINE);
+        while (true) {
+            try {
+                return validateAnswer(Console.readLine());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     private List<BuyingInformation> validateFormat(String input) {
