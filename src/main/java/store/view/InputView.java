38c51725d3bf0c6ecb5fc172a6fa871e9c74b30e
이준범 <jach1206@naver.com>
2024-11-10 20:16:46 +0900

    feat(view): 추가 구매할 상품이 있는지 입력 받는 메서드 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -12,6 +12,7 @@ public class InputView {
 
     private static final String BUYING_GUIDE_MESSAGE = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
     private static final String MEMBERSHIP_GUIDE_MESSAGE = "멤버십 할인을 받으시겠습니까? (Y/N)";
+    private static final String RETRY_GUIDE_MESSAGE = "감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)";
     private static final String SEPARATOR = ",";
     private static final String NEW_LINE = "\n";
     private static final String BUYING_INFORMATION_PATTERN = "\\[([가-힣]+)-([0-9]+)\\]";
@@ -33,6 +34,11 @@ public class InputView {
 
     public String inputMembership() {
         System.out.println(MEMBERSHIP_GUIDE_MESSAGE);
+        return validateAnswer(Console.readLine());
+    }
+
+    public String inputRetry() {
+        System.out.println(RETRY_GUIDE_MESSAGE);
         return validateAnswer(Console.readLine());
     }
 
