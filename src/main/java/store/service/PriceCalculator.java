8e93358741bc494a3fc218f9fe14443d8e036189
이준범 <jach1206@naver.com>
2024-11-11 03:36:28 +0900

    style: 일부 코드 주석 추가

--- src/main/java/store/service/PriceCalculator.java
+++ src/main/java/store/service/PriceCalculator.java
@@ -3,7 +3,6 @@ package store.service;
 import java.util.List;
 import store.domain.BuyingInformation;
 import store.domain.Products;
-import store.domain.Promotion;
 
 public class PriceCalculator {
     // 구매한 품목의 가격을 반환한다.
@@ -17,7 +16,7 @@ public class PriceCalculator {
         int totalPrice = 0;
         for (BuyingInformation information : buyingInformation) {
             int normalCount = information.getNormalCount();
-            normalCount += nonPromotionCount(information);
+//            normalCount += nonPromotionCount(information);
             int price = Products.getPrice(information.getName());
 
             totalPrice += price * normalCount;
@@ -25,13 +24,13 @@ public class PriceCalculator {
         return totalPrice;
     }
 
-    private static int nonPromotionCount(BuyingInformation buyingInformation) {
-        String promotionName = Products.getPromotion(buyingInformation.getName());
-        Promotion promotion = Promotion.checkPromotion(promotionName);
-
-        if (promotion == null) {
-            return 0;
-        }
-        return buyingInformation.getPromotionCount() % (promotion.getGet() + promotion.getBuy());
-    }
+//    private static int nonPromotionCount(BuyingInformation buyingInformation) {
+//        String promotionName = Products.getPromotion(buyingInformation.getName());
+//        Promotion promotion = Promotion.checkPromotion(promotionName);
+//
+//        if (promotion == null) {
+//            return 0;
+//        }
+//        return buyingInformation.getPromotionCount() % (promotion.getGet() + promotion.getBuy());
+//    }
 }
