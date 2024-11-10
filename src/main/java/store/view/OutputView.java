944d73679325fdd2f2e4486202816303b141d330
이준범 <jach1206@naver.com>
2024-11-10 23:47:17 +0900

    style: 출력형식에 맞춰 코드 개선

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -11,8 +11,8 @@ public class OutputView {
     private static final String WELCOME_MESSAGE = "안녕하세요. W편의점입니다.";
     private static final String PRODUCTS_GUIDE_MESSAGE = "현재 보유하고 있는 상품입니다.";
     private static final String START_OF_RECEIPT = "==============W 편의점================";
-    private static final String START_OF_BUYING_INFORMATION = "상품명\t\t수량\t금액";
-    private static final String START_OF_PROMOTION_INFORMATION = "=============증\t정===============";
+    private static final String START_OF_BUYING_INFORMATION = "상품명                수량       금액";
+    private static final String START_OF_PROMOTION_INFORMATION = "=============증     정===============";
     private static final String START_OF_TOTAL_INFORMATION = "====================================";
     private static final String SEPARATOR = "-";
     private static final String NEW_LINE = "\n";
@@ -32,9 +32,9 @@ public class OutputView {
 
     public void printBuyingInformationMessage(List<BuyingInformation> buyingInformation) {
         System.out.println(NEW_LINE + START_OF_RECEIPT);
-        System.out.println(START_OF_BUYING_INFORMATION);
+        System.out.printf("%-16s\t%-4s\t%-5s\n", "상품명", "수량", "금액");
         for (BuyingInformation information : buyingInformation) {
-            System.out.printf("%s\t\t%d\t%,d\n", information.getName(), information.getTotalCount(),
+            System.out.printf("%-16s\t%-4d\t%,-5d\n", information.getName(), information.getTotalCount(),
                     information.getTotalCount() * Products.getPrice(information.getName()));
         }
 
@@ -46,7 +46,7 @@ public class OutputView {
         List<Integer> count = totalPromotionInformation.getCount();
         for (int i = 0; i < name.size(); i++) {
             if (count.get(i) != 0) {
-                System.out.printf("%s\t\t%,d\n", name.get(i), count.get(i));
+                System.out.printf("%-16s\t%,-3d\n", name.get(i), count.get(i));
             }
         }
     }
@@ -57,9 +57,9 @@ public class OutputView {
         int totalPrice = totalPriceAndCount.getTotalPrice();
         int promotionSale = promotionInformation.getTotalPrice();
         int finalPrice = totalPrice - promotionSale - membershipSale;
-        System.out.printf("총구매액\t\t%d\t%,d\n", totalPriceAndCount.getCount(), totalPrice);
-        System.out.printf("행사할인\t\t\t-%,d\n", promotionSale);
-        System.out.printf("멤버십할인\t\t\t-%,d\n", membershipSale);
-        System.out.printf("내실돈\t\t\t%,d\n", finalPrice);
+        System.out.printf("%-16s\t%-4d\t%,-5d\n", "총구매액", totalPriceAndCount.getCount(), totalPrice);
+        System.out.printf("%-24s\t-%,-5d\n", "행사할인", promotionSale);
+        System.out.printf("%-24s\t-%,-5d\n", "멤버십할인", membershipSale);
+        System.out.printf("%-24s\t%,-5d\n", "내실돈", finalPrice);
     }
 }
