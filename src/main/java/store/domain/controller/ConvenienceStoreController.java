7bb0d70587ba38a9cf82b9cfb849b6e9edc8447c
이준범 <jach1206@naver.com>
2024-11-11 17:48:34 +0900

    refactor: 코드 구조 개선

--- src/main/java/store/domain/controller/ConvenienceStoreController.java
+++ src/main/java/store/domain/controller/ConvenienceStoreController.java
@@ -79,7 +79,6 @@ public class ConvenienceStoreController {
     public void askAdditionalItems() {
         String answer = inputView.inputRetry();
         if (answer.equals("Y")) {
-            initialize();
             List<BuyingInformation> buyingInformation = buyProduct();
             calculateProcess(buyingInformation);
             int membershipSale = applyMembership(buyingInformation);
