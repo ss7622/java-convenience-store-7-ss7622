4b6992638615b20fac8d3d49f45d377c88c93f38
이준범 <jach1206@naver.com>
2024-11-11 21:41:02 +0900

    style: 값을 재입력 받을 시, 안내 문구 다시 출력

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -23,9 +23,9 @@ public class InputView {
     }
 
     public String inputGetAdditionalProduct(String name, int count) {
-        System.out.println(NEW_LINE + "현재 " + name + "은(는) " + count + "개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
         while (true) {
             try {
+                System.out.println(NEW_LINE + "현재 " + name + "은(는) " + count + "개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
                 return validateAnswer(Console.readLine());
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
@@ -34,9 +34,10 @@ public class InputView {
     }
 
     public String inputNonPromotion(String name, int count) {
-        System.out.println(NEW_LINE + "현재 " + name + " " + count + "개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)");
         while (true) {
             try {
+                System.out.println(
+                        NEW_LINE + "현재 " + name + " " + count + "개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)");
                 return validateAnswer(Console.readLine());
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
@@ -45,9 +46,9 @@ public class InputView {
     }
 
     public String inputMembership() {
-        System.out.println(NEW_LINE + MEMBERSHIP_GUIDE_MESSAGE);
         while (true) {
             try {
+                System.out.println(NEW_LINE + MEMBERSHIP_GUIDE_MESSAGE);
                 return validateAnswer(Console.readLine());
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
@@ -56,9 +57,9 @@ public class InputView {
     }
 
     public String inputRetry() {
-        System.out.println(NEW_LINE + RETRY_GUIDE_MESSAGE);
         while (true) {
             try {
+                System.out.println(NEW_LINE + RETRY_GUIDE_MESSAGE);
                 return validateAnswer(Console.readLine());
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
